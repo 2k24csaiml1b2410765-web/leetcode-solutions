a307b0a46d4d2c44cb5b9848b5859a8fc809a89f
2k24csaiml1b2410765-web <2k24.csaiml1b.2410765@gmail.com>
2026-09-01 03:07:37 +0530

    Time: 0 ms (100%), Space: 9.8 MB (82.33%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+void getallSubsets(vector<int>&nums,vector<int>&ans,int i,vector<vector<int>>&allSubsets){
+    if(i==nums.size()){
+        allSubsets.push_back({ans});
+        return;
+    }
+    //inclusion
+    ans.push_back(nums[i]);
+    getallSubsets(nums,ans,i+1,allSubsets);
+    //backtracking
+    ans.pop_back();
+    //exclusion
+    getallSubsets(nums,ans,i+1,allSubsets);
+}
+    vector<vector<int>> subsets(vector<int>& nums) {
+      vector<vector<int>> allSubsets;
+      vector<int>ans;
+      getallSubsets(nums,ans,0,allSubsets);
+      return allSubsets;
+    }
+};
