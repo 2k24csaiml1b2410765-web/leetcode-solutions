b5b2cca15f66d49fc4942eb23f9d8e53dea29bcf
2k24csaiml1b2410765-web <2k24.csaiml1b.2410765@gmail.com>
2026-09-18 11:54:20 +0530

    Time: 0 ms (100%), Space: 7.7 MB (87.99%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int st=0;
+        int end=num;
+        while(st<=end){
+            long long int mid=st+(end-st)/2;
+            if(mid*mid==num){
+               
+                 return true;
+    
+                
+            }
+            if(mid*mid<num){
+                st=mid+1;
+                
+            }
+            else{
+                end=mid-1;
+            }
+           
+        }
+        return false;
+    }
+};
